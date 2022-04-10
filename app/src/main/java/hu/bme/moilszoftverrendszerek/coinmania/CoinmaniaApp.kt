c28ad7a9695290bda74a9bd733343cd2028d7785
barkasz <rbarkaszi@edu.bme.hu>
2022-04-10 21:02:55 +0200

    coinmania app init filies

---
+++ app/src/main/java/hu/bme/moilszoftverrendszerek/coinmania/CoinmaniaApp.kt
@@ -0,0 +1,7 @@
+package hu.bme.moilszoftverrendszerek.coinmania
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinmaniaApp :Application()
---
+++ app/src/main/java/hu/bme/moilszoftverrendszerek/coinmania/model/Coinmania.kt
@@ -0,0 +1,5 @@
+package hu.bme.moilszoftverrendszerek.coinmania.model
+
+data class Coinmania(
+    val name: String
+)
