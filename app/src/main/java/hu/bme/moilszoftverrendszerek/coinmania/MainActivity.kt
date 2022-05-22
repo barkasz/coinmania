06037d51b26e4a42639616eecf9c230a55d15496
barkasz <rbarkaszi@edu.bme.hu>
2022-05-22 21:27:36 +0200

    Google Analytics + Crashlytics integráció

--- app/src/main/java/hu/bme/moilszoftverrendszerek/coinmania/MainActivity.kt
+++ app/src/main/java/hu/bme/moilszoftverrendszerek/coinmania/MainActivity.kt
@@ -2,16 +2,25 @@ package hu.bme.moilszoftverrendszerek.coinmania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import hu.bme.moilszoftverrendszerek.coinmania.data.Coin
 import hu.bme.moilszoftverrendszerek.coinmania.data.AppDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    //analytics
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //analytics
+        firebaseAnalytics = Firebase.analytics
+        //
 
         btnSave.setOnClickListener {
             var newCoin = Coin(
@@ -25,39 +34,53 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnQuery.setOnClickListener {
-            queryGrades()
+            queryCoins()
         }
 
         btnDeleteAll.setOnClickListener {
             deleteAllGrades()
         }
 
+        btnTestCrash.setOnClickListener{
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
     }
 
     fun saveCoin(coin: Coin) {
         Thread {
-            AppDatabase.getInstance(this).gradeDao().insertCoins(coin)
+            AppDatabase.getInstance(this).coinDao().insertCoins(coin)
         }.start()
+
+        Thread {
+            //analytics
+            firebaseAnalytics.logEvent("save_button_clicked", null)
+        }
     }
 
-    fun queryGrades(){
+    fun queryCoins(){
         Thread {
-            val grades = AppDatabase.getInstance(this).gradeDao().
+            val grades = AppDatabase.getInstance(this).coinDao().
             getAllCoins()
 
             runOnUiThread{
                 tvResult.text = ""
                 grades.forEach{
-                    tvResult.append("Name: ${it.coinName} grade: ${it.price}\n")
+                    tvResult.append("Name: ${it.coinName} price: ${it.price}\n")
                 }
             }
 
+        }.start()
+
+        Thread {
+            //analytics integráció
+            firebaseAnalytics.logEvent("query_coins", null)
         }.start()
     }
 
     private fun deleteAllGrades() {
         Thread {
-            AppDatabase.getInstance(this).gradeDao().deleteAllCoins()
+            AppDatabase.getInstance(this).coinDao().deleteAllCoins()
         }.start()
     }
 
--- app/src/main/java/hu/bme/moilszoftverrendszerek/coinmania/data/AppDatabase.kt
+++ app/src/main/java/hu/bme/moilszoftverrendszerek/coinmania/data/AppDatabase.kt
@@ -8,7 +8,7 @@ import androidx.room.RoomDatabase
 @Database(entities = [Coin::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract fun gradeDao(): CoinDAO
+    abstract fun coinDao(): CoinDAO
 
     companion object {
         @Volatile
